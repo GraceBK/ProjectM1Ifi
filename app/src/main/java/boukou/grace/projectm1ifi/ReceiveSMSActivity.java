2f95bfd514ab210e5eb76bd14c0fecc0c761d793
GraceBK <iris.b1991@gmail.com>
2018-05-26 19:45:47 +0200

    Réfactoring

---
+++ app/src/main/java/boukou/grace/projectm1ifi/ReceiveSMSActivity.java
@@ -0,0 +1,13 @@
+package boukou.grace.projectm1ifi;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReceiveSMSActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_receive_sms);
+    }
+}
